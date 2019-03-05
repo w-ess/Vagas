b96b071c6947306318ad0baf93f112e8157f5cd2
wesley <w-ess@hotmail.com>
2019-03-05 16:28:26 -0300

    ediçao de vagas

---
+++ Vagas/Vagas/Paginas/EditarVaga.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vagas.Banco;
+using Vagas.Modelos;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Vagas.Paginas
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class EditarVaga : ContentPage
+	{
+        private Vaga vaga { get; set; }
+		public EditarVaga (Vaga vaga)
+		{
+			InitializeComponent ();
+
+            this.vaga = vaga;
+
+            NomeVaga.Text = vaga.NomeVaga;
+            Empresa.Text = vaga.Empresa;
+            Quantidade.Text = vaga.Quantidade.ToString();
+            Cidade.Text = vaga.Cidade;
+            Salario.Text = vaga.Salario.ToString();
+            Descricao.Text = vaga.Descricao;
+            TipoContratacao.IsToggled = vaga.TipoContratacao != "CLT";
+            Telefone.Text = vaga.Telefone;
+            Email.Text = vaga.Email;
+        }
+
+        public void SalvarAction(object sender, EventArgs args)
+        {
+            vaga.NomeVaga = NomeVaga.Text;
+            vaga.Quantidade = short.Parse(Quantidade.Text);
+            vaga.Salario = double.Parse(Salario.Text);
+            vaga.Empresa = Empresa.Text;
+            vaga.Cidade = Cidade.Text;
+            vaga.Descricao = Descricao.Text;
+            vaga.TipoContratacao = (TipoContratacao.IsToggled) ? "PJ" : "CLT";
+            vaga.Telefone = Telefone.Text;
+            vaga.Email = Email.Text;
+
+            Database dataBase = new Database();
+            dataBase.Atualizacao(vaga);
+
+            App.Current.MainPage = new NavigationPage(new MinhasVagasCadastradas());
+        }
+
+    }
+}
--- Vagas/Vagas/Paginas/MinhasVagasCadastradas.xaml.cs
+++ Vagas/Vagas/Paginas/MinhasVagasCadastradas.xaml.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Vagas.Banco;
+using Vagas.Modelos;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,19 +13,37 @@ namespace Vagas.Paginas
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class MinhasVagasCadastradas : ContentPage
 	{
-		public MinhasVagasCadastradas ()
+        List<Vaga> Lista { get; set; }
+        public MinhasVagasCadastradas ()
 		{
 			InitializeComponent ();
-		}
+            ConsultarVagas();
+        }
+
+        private void ConsultarVagas()
+        {
+            Database database = new Database();
+            Lista = database.Consultar();
+            ListaVagas.ItemsSource = Lista;
+            lblCount.Text = Lista.Count.ToString();
+        }
 
         public void EditarAction(object sender, EventArgs args)
         {
+            Label lblDetalhe = (Label)sender;
+            Vaga vaga = ((TapGestureRecognizer)lblDetalhe.GestureRecognizers[0]).CommandParameter as Vaga;
 
+            Navigation.PushAsync(new EditarVaga(vaga));
         }
 
         public void ExcluirAction(object sender, EventArgs args)
         {
+            Label lblExcluir = (Label)sender;
+            Vaga vaga = ((TapGestureRecognizer)lblExcluir.GestureRecognizers[0]).CommandParameter as Vaga;
 
+            Database database = new Database();
+            database.Exclusao(vaga);
+            ConsultarVagas();
         }
     }
 }
